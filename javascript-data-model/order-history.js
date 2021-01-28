b7a2ab17b33a98466b1884f080a6e2711e41161a
Mohamed Elmessiry <mohamedelmessiry@hotmail.com>
2021-01-28 18:03:02 -0500

    changed the format

--- javascript-data-model/order-history.js
+++ javascript-data-model/order-history.js
@@ -3,35 +3,46 @@ var orderHistory = [
 
   {
     orderPlaced: 'August 4, 2020',
-    total: '$34',
     shipTo: 'JS Masher',
     order: '114-3941689-877232',
-    deliveredDate: 'Aug 8, 2020',
-    bookTitle: 'Javscript for Impantient programmers',
-    bookAuthor: 'Rauschmayer, Dr.Axel',
-    returnWindowClosingDate: 'Sep 7, 2020',
-    price: '$31.55'
+    items: [
+      {
+        deliveredDate: 'Aug 8, 2020',
+        bookTitle: 'Javscript for Impantient programmers',
+        bookAuthor: 'Rauschmayer, Dr.Axel',
+        returnWindowClosingDate: 'Sep 7, 2020',
+        price: '$31.55'
+      }
+    ]
+
   },
   {
     orderPlaced: 'July 19,2020',
-    total: '$44.53',
     shipTo: 'JS Masher',
     order: '113-9984268-1280257',
-    deliveredDate: 'Jul 20,2020',
-    bookTitle: 'The Timeless Way of Building',
-    bookAuthor: 'Alexander, Christopher',
-    returnWindowClosingDate: 'Aug 19 2020',
-    price: '$41.33'
+    items: [
+      {
+        deliveredDate: 'Jul 20,2020',
+        bookTitle: 'The Timeless Way of Building',
+        bookAuthor: 'Alexander, Christopher',
+        returnWindowClosingDate: 'Aug 19 2020',
+        price: '$41.33'
+      }
+    ]
+
   },
   {
     orderPlaced: 'July 4,2020',
-    total: '$17.22',
     shipTo: 'JS Masher',
     order: '114-2875557-9059049',
-    deliveredDate: 'Jul 7,2020',
-    itemName: 'Gamecube Controller Adapter, Super Smash Bros Switch Gamcube Adapter for WII U,PC Support Turbo and Vibration Features. No Driver and No Lag-Gamecube Adapter',
-    returnWindowClosingDate: 'Aug 5, 2020',
-    price: '$15.98'
+    items: [
+      {
+        deliveredDate: 'Jul 7,2020',
+        itemName: 'Gamecube Controller Adapter, Super Smash Bros Switch Gamcube Adapter for WII U,PC Support Turbo and Vibration Features. No Driver and No Lag-Gamecube Adapter',
+        returnWindowClosingDate: 'Aug 5, 2020',
+        price: '$15.98'
+      }
+    ]
   },
 
   {
