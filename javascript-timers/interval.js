80d8978e51e2bfed50b52c9a2c681c9cfb7b390f
Mohamed Elmessiry <mohamedelmessiry@hotmail.com>
2021-02-17 14:52:39 -0500

    updated if statement.

--- javascript-timers/interval.js
+++ javascript-timers/interval.js
@@ -3,11 +3,12 @@ var heading = document.querySelector('h1');
 function update() {
   var counter = 4;
   var countdown = setInterval(function () {
-    heading.textContent = counter;
-    counter--;
     if (counter === 0) {
       heading.textContent = '~Earth Beeeelooowww Us~';
       clearInterval(countdown);
+    } else {
+      heading.textContent = counter;
+      counter--;
     }
   }, 2000);
 
