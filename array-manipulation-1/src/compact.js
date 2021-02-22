d0c5305c11d59c67ee5586882cb0a38d6fe86b4d
Mohamed Elmessiry <mohamedelmessiry@hotmail.com>
2021-02-21 21:06:23 -0500

    Completed array manipulation 1.

--- array-manipulation-1/src/compact.js
+++ array-manipulation-1/src/compact.js
@@ -1,1 +1,10 @@
-/* exported compact */
+function compact(array) {
+  var n = [];
+  for (var i = 0; i <= array.length; i++) {
+    if (array[i]) {
+      n.push(array[i]);
+    }
+
+  }
+  return n;
+}
