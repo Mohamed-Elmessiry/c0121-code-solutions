0ae9939ec2eed98e8e8b79680886e1aee4119d0b
Mohamed Elmessiry <mohamedelmessiry@hotmail.com>
2021-03-01 15:24:58 -0500

    added arrays of objects to hold the product name

--- javascript-data-model/order-history.js
+++ javascript-data-model/order-history.js
@@ -2,28 +2,27 @@ var orderHistory = [
   {
     orderPlaced: 'August 4, 2020',
     total: '$34',
-    product: 'JavaScript for impatient programmers',
+    product: [{ name: 'JavaScript for impatient programmers' }],
     author: 'Rauschmayer, Dr.Axel',
     shipTo: 'JS Masher'
   },
   {
     orderPlaced: 'July 19, 2020',
     total: '$44.53',
-    product: 'The Timeless Way of Building',
+    product: [{ name: 'The Timeless Way of Building' }],
     author: 'Alexander Christopher',
     shipTo: 'JS Masher'
   },
   {
     orderPlaced: 'July 4, 2020',
     total: '$17.22',
-    product: 'Gamecube Contoller Adapter, Super Smash Bros Switch Gamecube Adapter for  WII U, PC Support Turbo and Vibration Features. No Driver and No Lag- Gamecube Adapter',
+    product: [{ name: 'Gamecube Contoller Adapter, Super Smash Bros Switch Gamecube Adapter for  WII U, PC Support Turbo and Vibration Features. No Driver and No Lag- Gamecube Adapter' }],
     shipTo: 'JS Masher'
   },
   {
     orderPlaced: 'July 3, 2020',
     total: '$138.93',
-    product: 'GameCube Controller- Super Smash Bros Edition (Nintendo Switch)',
-    product2: 'The Art of Sql',
+    product: [{ name: 'GameCube Controller- Super Smash Bros Edition (Nintendo Switch)' }, { name: 'The Art of Sql' }],
     shipTo: 'JS Masher'
   }
 
