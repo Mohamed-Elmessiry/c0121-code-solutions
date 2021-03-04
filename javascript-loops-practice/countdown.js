c906cc7f8ce1d53c3abd35865bf081538f37c4eb
Mohamed Elmessiry <mohamedelmessiry@hotmail.com>
2021-03-03 19:49:52 -0500

    modified to i

--- javascript-loops-practice/countdown.js
+++ javascript-loops-practice/countdown.js
@@ -1,10 +1,9 @@
 /* exported countdown */
 
 function countdown(number) {
-  debugger;
   var arr = [];
   for (var i = number; i >= 0; i--) {
-    arr.push(number--);
+    arr.push(i);
   }
   return arr;
 }
